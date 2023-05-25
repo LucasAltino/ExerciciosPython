c21575898d254cd1f370c78e3c61cdca89e69d8a
LucasAltino <124930759+LucasAltino@users.noreply.github.com>
2023-05-25 10:53:32 -0300

    Delete ex3.py

--- ex3.py
+++
@@ -1,5 +0,0 @@
-salario = float(input("Digite o valor do seu salário: "))
-aumento = float(input("Digite o pencentual de aumento:"))
-novo = salario + (salario/100 * aumento)
-
-print("O valor de seu salário será de:", novo)

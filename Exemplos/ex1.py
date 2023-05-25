755be28982351470c2adfd71a16593a1437f92cf
LucasAltino <124930759+LucasAltino@users.noreply.github.com>
2023-05-25 10:54:31 -0300

    Delete Exemplos directory

--- Exemplos/ex1.py
+++
@@ -1,8 +0,0 @@
-P1 = 10
-P2 = 7
-P3 = 8
-
-media = (P1 + P2 + P3) / 3
-
-print("A media dos alunos é", media)
-
--- Exemplos/ex4.py
+++
@@ -1,6 +0,0 @@
-PI = 3.1415
-r = float(input("Digite o raio: "))
-altura = float(input("Digite a altura: "))
-volume = PI * r**2 * altura
-
-print(f"O volume é de: {volume:.2f}")
